c02ec9f91198de1245478df04c8814fc83885f91
yunkii <degood271@gmail.com>
2018-08-27 00:46:34 +0200

    add Search

--- P08/src/Search.js
+++ P08/src/Search.js
@@ -1,32 +1,39 @@
-import React from 'react'
-import {PropTypes} from 'prop-types'
-
+import React from 'react';
+import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
 class Search extends React.Component {
+    static propTypes = {
+        locations: PropTypes.array.isRequired,
+        query: PropTypes.string.isRequired
+    }
 
-  componentDidMount(){
-    console.log('Search function starts');
-  }
+    state = {
+        query: ''
+    }
 
+    // Update query everytime user enter the character.
+    updateQuery = (query) => {
+        if (query) {
+          const match = new RegExp(escapeRegExp(query),'i')
+          this.props.filteredLocations = this.props.locations.filter((location)=> match.test(location.name))
+        } else {
+          this.props.filteredLocations = this.props.locations;
+        }
+    }
 
-
-  state = {
-    query: '',
-  }
-
-
-render() {
-
-    return (
-                <input role="Search" 
-                       aria-labelledby="filter"
-                       className='search-input'
-                       type='text'
-                       placeholder='Enter an attraction'
-                       value={this.state.query}
-                       onChange={this.updateSearchQuery}/>
-	)
-}
-
+    render() {
+        return (
+                <input 
+                role="Search" 
+                aria-label="search location" 
+                className='search-input'
+                type='text'
+                placeholder='Enter an attraction'
+                value={this.state.query}
+                onChange={(event => this.updateQuery(event.target.value))}
+                />
+            )
+    }
 }
 
 export default Search;
